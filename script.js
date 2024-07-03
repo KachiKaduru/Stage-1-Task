9050ee966f88737a7398ae423290cb100622fb3c
Kachii <kachikaduru@gmail.com>
2024-07-03 02:34:02 +0100

    This is the Stage 1 Task for the HNG 11 Internship for #frotend. Hope it goes well

---
+++ script.js
@@ -0,0 +1,9 @@
+function updateTimeAndDay() {
+  const currentTimeUTC = new Date().toUTCString();
+  const currentDay = new Date().toLocaleDateString("en-US", { weekday: "long" });
+
+  document.querySelector('[data-testid="currentTimeUTC"]').textContent = currentTimeUTC;
+  document.querySelector('[data-testid="currentDay"]').textContent = currentDay;
+}
+
+window.onload = updateTimeAndDay;
